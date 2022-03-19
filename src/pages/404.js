59ab390ee40a9a8d57d407ef7ed1f67cd538c493
darkh0le <andresivan.aldama@gmail.com>
2022-03-18 20:21:36 -0600

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Link from "gatsby-link";
+
+export default function NotFoundPage() {
+    return (
+        <main>Not Found Page: HTTP Code: 404
+            <Link to="/">The Mugen Dev</Link>
+        </main>
+    )
+}
